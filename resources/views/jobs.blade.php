155f16e718dced9d9a0084326d33ad75bcc15892
Ahmed-rbr <ahmed.rabar.dev@gmail.com>
2025-10-15 20:01:09 +0100

    understanding eager loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,9 +2,21 @@
      <x-slot:heading>
         About Page
 </x-slot:heading>
-<ul class="flex flex-col gap-5  ">
- @foreach ($jobs as $job )
-    <li class="bg-blue-400 hover:underline  p-6 rounded-2xl"><a href="/jobs/ {{ $job['id']}} ">       <strong>{{$job['title']}}</strong>: Pays {{$job['salary']}} per year</li></a>
+<div class="flex flex-col gap-5  ">
 
-    @endforeach</ul>
+ @foreach ($jobs as $job )
+
+
+   <a class=" block border-gray-200 border   p-6 rounded-2xl" href="/jobs/ {{ $job['id']}} "> 
+     <div class="font-bold">
+{{ $job->employer->name }}   
+</div>
+
+      <div class="hover:underline">   <strong>{{$job['title']}}</strong>: Pays {{$job['salary']}} per year
+</div>
+    
+
+</a>
+
+    @endforeach</div>
 </x-layout>
